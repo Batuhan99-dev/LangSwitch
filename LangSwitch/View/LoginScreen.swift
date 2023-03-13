c161095916027ced4bf9618e7c32b022be84017b
Batuhan <batu.omer@icloud.com>
2023-03-13 23:25:28 +0300

    Initial Commit

---
+++ LangSwitch/View/LoginScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LoginScreen.swift
+//  LangSwitch
+//
+//  Created by Batuhan Yılmaz on 13.03.2023.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+ 
+
+}
---
+++ LangSwitch/View/SigninScreen.swift
@@ -0,0 +1,21 @@
+//
+//  SigninScreen.swift
+//  LangSwitch
+//
+//  Created by Batuhan Yılmaz on 13.03.2023.
+//
+
+import UIKit
+
+class SigninScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
