2f023968968c5dbfcfa9dfb2f22ecef37d4ff7ae
Batuhan <batu.omer@icloud.com>
2023-03-14 01:23:01 +0300

    Add Firebase

--- LangSwitch/AppDelegate.swift
+++ LangSwitch/AppDelegate.swift
@@ -6,6 +6,13 @@
 //
 
 import UIKit
+import SwiftUI
+import FirebaseCore
+import FirebaseFirestore
+import FirebaseAuth
+
+      
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +20,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        Firebase setup
+        FirebaseApp.configure()
+        let db = Firestore.firestore()
+        print(db)
         // Override point for customization after application launch.
         return true
     }
